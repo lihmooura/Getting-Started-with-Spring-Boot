4b4374f673a61dab7752e0a4bc84b5249c5a1702
Elise Melo <elisemelo@Elises-MacBook-Air.local>
2022-06-20 15:29:38 -0300

    update info

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,7 +1,10 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,5 +43,24 @@ public class StudentService {
 		}
 		studentRepository.deleteById(studentId);
 		
+	}
+
+
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email) {
+		Student student = studentRepository.findById(studentId)
+				.orElseThrow(() -> new IllegalStateException (
+						"student with id" + studentId + "does not exist"));
+				if (name != null &&
+						name.length() > 0 &&
+						!Objects.equals(student.getName(), name)) {
+					student.setName(name);
+				}
+		
+				if (email != null &&
+						email.length() > 0 &&
+						!Objects.equals(student.getEmail(), email)) {
+					
+				}
 	}
 }
